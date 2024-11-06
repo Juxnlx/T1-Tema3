8365abb23f24f4d621972c3b7d9660d6d95a5183
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-06 12:41:22 +0100

    Ejercicio 5 - Completo

--- Tema3/src/boletinpart1/Ejercicio5.java
+++ Tema3/src/boletinpart1/Ejercicio5.java
@@ -18,11 +18,15 @@ public class Ejercicio5 {
 		// valores introducidos por el usuario.
 		float num;
 
-		// Creamos la variable max como float para almacenar el maximo posible.
-		float max = Float.MAX_VALUE;
+		// Creamos la variable max como float para almacenar el maximo posible. Lo
+		// debemos inicializar al minimo posible para que asi cualquier número que
+		// introduzcamos sea mayor.
+		float max = Float.MIN_VALUE;
 
-		// Creamos la variable min como float para almacenar el minimo posible.
-		float min = Float.MIN_VALUE;
+		// Creamos la variable min como float para almacenar el minimo posible. Lo
+		// debemos de inicializar al mayor número posible para que así cualquier número
+		// que introduzcamos sea menor.
+		float min = Float.MAX_VALUE;
 
 		// Creamos la variable suma como float para almacenar la suma de todos los
 		// números introducidos en la tabla.
@@ -44,18 +48,25 @@ public class Ejercicio5 {
 			// En cada iteración vamos sumando cada valor introducido a la tabla.
 			suma += tablaNum[i];
 
-			// Comprobamos si el número max es mayor o igual que el número introducido. Si
-			// es así asignamos a max el número.
-			if (max >= num) {
+			// Comprobamos si el número es mayor o igual al max si es así, asignamos a max
+			// el número.
+			if (num >= max) {
 				max = num;
 			}
 
-			// Comprobamos si el número min es menor o igual que el número introducido. Si
-			// es así asignamos a min el número.
-			if (min <= num) {
+			// Comprobamos si el número es menor o igual al min si es así, asignamos a min
+			// el número.
+			if (num <= min) {
 				min = num;
 			}
 		}
+
+		// Imprimimos la suma de todos los números de la tabla y mostramos el minimo y
+		// el maximo.
+		System.out.println();
+		System.out.println("La suma de todos los números de la tabla es: " + suma);
+		System.out.println("El máximo número de la tabla es: " + max);
+		System.out.println("El mínimo número de la tabla es: " + min);
 
 		// Cierre de Scanner
 		sc.close();
