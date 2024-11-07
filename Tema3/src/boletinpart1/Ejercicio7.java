4cb8d28a753b40c4abd37ab120dad76c64e06153
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-07 12:17:27 +0100

    Ejercicio 7 - Completo

--- Tema3/src/boletinpart1/Ejercicio7.java
+++ Tema3/src/boletinpart1/Ejercicio7.java
@@ -37,9 +37,11 @@ public class Ejercicio7 {
 		// Mostramos un mensaje diciendo lo que vamos a mostrar a continuación.
 		System.out.println("Grafico de barras temperatura media de cada mes --> ");
 
-		// Creamos este for para imprimir tantos asteriscos como tenga cada posición de
-		// la tabla.
+		// Creamos este for para leer todos los valores de la tabla.
 		for (int i = 0; i < tablaTemp.length; i++) {
+			// Con este switch comprobamos en que posición estamos de la tabla y dependiendo
+			// de la posición en la que nos escontremos sera un mes u otro. Y lo imprimimos
+			// por consola.
 			switch (i) {
 			case 0 -> System.out.print("Enero:      | ");
 			case 1 -> System.out.print("Febrero:    | ");
@@ -55,11 +57,19 @@ public class Ejercicio7 {
 			case 11 -> System.out.print("Diciembre:  | ");
 			}
 
+			// Con este for recorremos desde uno hasta el valor de la posición en la que nos
+			// encontremos en la iteración e imprime tantos asteriscos como valor de dicha
+			// posición.
 			for (int j = 1; j <= tablaTemp[i]; j++) {
 				System.out.print("* ");
 			}
+
+			// Salto de linea entre linea y linea.
 			System.out.println();
 		}
+
+		// Cierre de Scanner
+		sc.close();
 	}
 
 }
