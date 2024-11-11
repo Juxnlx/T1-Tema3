909e66a8fac05fc250914ee3c839f5a1bb895c2a
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-11 18:18:57 +0100

    Ejercicio 6 - Corregido y completo

--- Tema3/src/boletinpart2/Ejercicio6.java
+++ Tema3/src/boletinpart2/Ejercicio6.java
@@ -17,7 +17,7 @@ public class Ejercicio6 {
 		int tablaPremio[] = { 4, 12, 19, 26, 32, 43 };
 
 		// Creamos el array tablaIntento como int de tamaño 6 para almacenar la
-		// combinación de números aleatorios entre 1 y 9.
+		// combinación de números aleatorios entre 1 y 49.
 		int tablaIntento[] = new int[6];
 
 		// Creamos la variable cont como int para contabilizar el número de aciertos del
@@ -32,11 +32,12 @@ public class Ejercicio6 {
 		Arrays.sort(tablaIntento);
 
 		// Creamos este for para recorrer la tablaIntento y rellenarla con números
-		// aleatorios entre 1 y 9.
+		// aleatorios entre 1 y 49.
 		for (int i = 0; i < tablaIntento.length; i++) {
 			tablaIntento[i] = rand.nextInt(1, 50);
-			// Comprobamos si los números de los dos boletos en la misma posición son
-			// iguales o no, si es así el contador de aciertos (cont) le sumamos 1.
+			// Comprobamos si cada número de la tablaIntento se encuentra en la tablaPremio,
+			// si es así nos devuelve la posición que siempre sera mayor o igual a 0 y el
+			// contador +1, si no prueba con otro número.
 			if (Arrays.binarySearch(tablaPremio, tablaIntento[i]) >= 0) {
 				cont++;
 			}
