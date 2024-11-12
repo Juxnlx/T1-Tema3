de21869b812b05c11ad1a3aa99015a2da7268061
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-12 20:04:30 +0100

    Ejercicio 2 - Completo

--- Tema3/src/englishexercises/Ejercicio2.java
+++ Tema3/src/englishexercises/Ejercicio2.java
@@ -1,18 +1,75 @@
 package englishexercises;
 
-/**
- * Escriba un programa que permita al usuario almacenar una secuencia aritmética
- * en una matriz y luego el programa la mostrará en la pantalla. Una secuencia
- * aritmética es una serie de números que comienza con un valor inicial V, y
- * luego continúa con incrementos de un valor I. Por ejemplo, con V=1 e I=2 la
- * secuencia sería: 1, 3, 5, 7, 9… Con V=7 e I=10, la secuencia sería: 7, 17,
- * 27, 37 El programa preguntará al usuario los valores V e I, además del número
- * de valores a crear (la longitud del array).
- */
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// We declare the arithmeticSequence array as int to later create our table with
+		// the indicated size.
+		int arithmeticSequence[];
+
+		// We create the variable tableSize as int to store the size that our table will
+		// have.
+		int tableSize;
+
+		// We create the variable initialValue as int to store the initial value entered
+		// by the user.
+		int initialValue;
+
+		// We create the variable increase as int to store the value that we must
+		// increase to the initial value.
+		int increase;
+
+		// We create the variable increase Value as int to store the value to be entered
+		// in each position of the table.
+		int increasedValue;
+
+		// We create the Scanner to read the table size, the initial value and the
+		// increment.
+		Scanner sc = new Scanner(System.in);
+
+		// We ask the user to enter the size of the table and read it.
+		System.out.print("Enter the size of the table --> ");
+		tableSize = sc.nextInt();
+
+		// We create the arithmeticSequence table with a size entered by the user to
+		// store the sequence of numbers.
+		arithmeticSequence = new int[tableSize];
+
+		// We ask the user to enter the initial value with which to start the sequence
+		// of numbers and we read it.
+		System.out.print("Enter the initial value --> ");
+		initialValue = sc.nextInt();
+
+		// We ask the user to enter the increment that our table will have.
+		System.out.print("Enter the increment of our table --> ");
+		increase = sc.nextInt();
+
+		// We assign to the variable increasedValue the starting value entered by the
+		// user.
+		increasedValue = initialValue;
+
+		// With this for we go through the entire table and in each iteration we add the
+		// numbers.
+		for (int i = 0; i < arithmeticSequence.length; i++) {
+			// We assign the incremented value to the table position.
+			arithmeticSequence[i] = increasedValue;
+
+			// On each turn we add to the increased value the increment entered by the user
+			// previously.
+			increasedValue += increase;
+		}
+
+		// Line break and explanatory message.
+		System.out.println();
+		System.out.print("Secuencia de números --> ");
+		System.out.println(Arrays.toString(arithmeticSequence));
+
+		// Closing Scanner
+		sc.close();
 
 	}
 
