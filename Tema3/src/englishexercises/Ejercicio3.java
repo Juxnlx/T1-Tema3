504181ed182ae48e27bfa8878737c53abcb95162
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-12 20:04:43 +0100

    Ejercicio 3 - Incompleto

---
+++ Tema3/src/englishexercises/Ejercicio3.java
@@ -0,0 +1,28 @@
+package englishexercises;
+
+/**
+ * ¿Dónde está la mosca? Estamos intentando atrapar una mosca. La mosca elegirá
+ * aleatoriamente una posición en nuestro array y tendremos que adivinar la
+ * posición en la que se encuentra la mosca. El programa le preguntará al
+ * usuario la posición en la que cree que está la mosca. Si la posición es
+ * correcta, entonces el jugador gana y el programa termina. Si no es correcta,
+ * entonces pueden suceder dos cosas: 
+ * 
+ * La mosca está en una posición contigua.
+ * Entonces vuela a otra posición. El programa debe decirle al jugador que
+ * estaba cerrado pero la mosca cambió su posición.
+ * 
+ * La mosca no está en una posición contigua, entonces la mosca se queda donde
+ * está. El programa debe decirle al jugador que la mosca está lejos de esa
+ * posición.
+ * 
+ * El tamaño del array será 10, pero se puede cambiar.
+ */
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
