c8682b6cd79e9e4f456cc81fbb2b363c66d9337a
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-12 20:24:25 +0100

    Ejercicio 6 - Corregido

--- Tema3/src/boletinpart2/Ejercicio6.java
+++ Tema3/src/boletinpart2/Ejercicio6.java
@@ -20,26 +20,52 @@ public class Ejercicio6 {
 		// combinación de números aleatorios entre 1 y 49.
 		int tablaIntento[] = new int[6];
 
-		// Creamos la variable cont como int para contabilizar el número de aciertos del
-		// boleto aleatorio.
-		int cont = 0;
-
 		// Creamos la clase Random para generar números aleatorios para la TablaIntento.
 		Random rand = new Random();
 
-		// Ordenamos la tablaPremio de forma ascendente con la clase Arrays y usamos el
-		// sort.
-		Arrays.sort(tablaIntento);
+		// Creamos la variable contAciertos como int para contabilizar el número de
+		// aciertos del boleto aleatorio.
+		int contAciertos = 0;
 
-		// Creamos este for para recorrer la tablaIntento y rellenarla con números
-		// aleatorios entre 1 y 49.
-		for (int i = 0; i < tablaIntento.length; i++) {
-			tablaIntento[i] = rand.nextInt(1, 50);
-			// Comprobamos si cada número de la tablaIntento se encuentra en la tablaPremio,
-			// si es así nos devuelve la posición que siempre sera mayor o igual a 0 y el
-			// contador +1, si no prueba con otro número.
-			if (Arrays.binarySearch(tablaPremio, tablaIntento[i]) >= 0) {
+		// Creamos la variable valorAleatorio como int para almacenar el valor que nos
+		// va generando el random
+		int valorAleatorio;
+
+		// Creamos la varible posBuscada como int para almacenar la posición de un
+		// número si es que se encuentra en nuestra tablaIntento.
+		int posBusqueda;
+
+		// Creamos la variable cont como int para contabilizar los números aleatorios no
+		// repetidos se guardan en nuestra tabla.
+		int cont = 0;
+
+		// Comprobamos que el contador siempre sea 6 para que siempre se generen 6
+		// números aleatorios que no se repitan.
+		while (cont < 6) {
+			// A nuestra variable valorAleatorio le asignamos un número aleatorio entre 1 y
+			// 49 incluidos.
+			valorAleatorio = rand.nextInt(1, 50);
+			// Ordenamos la tabla.
+			Arrays.sort(tablaIntento);
+			// En la variable posBusqueda almacenamos la posición donde se encuentra el
+			// número en nuestra tabla de random si es que se encuentra.
+			posBusqueda = Arrays.binarySearch(tablaIntento, valorAleatorio);
+			// Comprobamos si la posición es negativa, si es así es que no se ha encontrado
+			// por lo tanto a la primera posición de la tabla le asignamos el valor
+			// aleatorio y el contador lo ponemos a +1.
+			if (posBusqueda < 0) {
+				tablaIntento[0] = valorAleatorio;
 				cont++;
+			}
+		}
+
+		// COn este for recorremos la tabla con todos los random y vamos comprobando 1 a
+		// 1 si se encuentra en la tabla premiada. Si es así devuelve una posición
+		// positiva entra en el if y el contador de aciertos lo pone a +1.
+		for (int valor : tablaIntento) {
+			posBusqueda = Arrays.binarySearch(tablaPremio, valor);
+			if (posBusqueda >= 0) {
+				contAciertos++;
 			}
 		}
 
@@ -48,7 +74,7 @@ public class Ejercicio6 {
 		System.out.println("Boleto premiado -->  " + Arrays.toString(tablaPremio));
 		System.out.println("Boleto aleatorio --> " + Arrays.toString(tablaIntento));
 
-		System.out.println("Numero de aciertos: " + cont);
+		System.out.println("Numero de aciertos: " + contAciertos);
 
 	}
 
