d673557496072cf7ed30b19bc20a047d5828c25d
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2024-11-13 09:27:30 +0100

    Ejercicio 3 - Completo

--- Tema3/src/boletinpart3/Ejercicio3.java
+++ Tema3/src/boletinpart3/Ejercicio3.java
@@ -1,30 +1,55 @@
 package boletinpart3;
 
+import java.util.Scanner;
+
 /**
- * Este programa crea una tabla bidimensonal de tamaño diez e imprime las tablas
- * de multiplicar del 1 al 10.
+ * Este programa pide las dimensiones de la tabla por teclado y crea la tabla.
+ * Ahora rellena la tabla de la sigueinte forma, cada posición contrendra el
+ * valor 10 * i + j.
  */
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		
-		// Creamos el array tablaMult como int para almacenar las tablas de multiplicar
-		// en nuestra de tabla con 10 filas y 10 columnas.
-		int tablaMult[][] = new int[10][10];
+
+		// Declaramos el array tablaSecuencia como int para almacenar una secuencia de
+		// valores.
+		int tablaSecuencia[][];
+
+		// Creamos la variable fila como int para almacenar el número de filas de
+		// nuestra tabla.
+		int fila;
+
+		// Creamos la variable columna como int para almacenar el número de columnas de
+		// nuestra tabla.
+		int columna;
+
+		// Creamos el Scanner para leer la fila y la columna por teclado.
+		Scanner sc = new Scanner(System.in);
+
+		// Le pedimos al usuario que introduzca el valor de la fila y la leemos.
+		System.out.print("Introduce el valor de la fila --> ");
+		fila = sc.nextInt();
+
+		// Le pedimos al usuario que introduzca el valor de la columna y la leemos.
+		System.out.print("Introduce el valor de la columna --> ");
+		columna = sc.nextInt();
+
+		// Creamos la tabla con las dimensiones introducidas por el usuario.
+		tablaSecuencia = new int[fila][columna];
 
 		// Creamos este for para recorrer las filas de nuestra tabla
-		for (int i = 0; i < tablaMult.length; i++) {
+		for (int i = 0; i < tablaSecuencia.length; i++) {
 			// Creamos este for para recorrer las posciones de cada fila (columanas).
-			for (int j = 0; j < tablaMult[0].length; j++) {
-				// Asignamos a cada posición de la tabla la correspondiente multiplicación entre
-				// la posición +1 de la fila y columna.
-				tablaMult[i][j] = (i + 1) * (j + 1);
-				// Imprimimos la posición mas un espacio entre valor y valor.
-				System.out.print(tablaMult[i][j] + "\t");
+			for (int j = 0; j < tablaSecuencia[0].length; j++) {
+				tablaSecuencia[i][j] = 10*i+j;
+				System.out.print(tablaSecuencia[i][j] + "\t");
 			}
-			//Salto de linea
+			// Salto de linea
 			System.out.println();
 		}
+		
+		//Cierre de Scanner
+		sc.close();
 
 	}
 
