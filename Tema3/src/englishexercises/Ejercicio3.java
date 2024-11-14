b417f55b29cd81583b6170de179b70766dd0b724
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-11-14 11:51:17 +0100

    Ejercicio 3 - Completo

--- Tema3/src/englishexercises/Ejercicio3.java
+++ Tema3/src/englishexercises/Ejercicio3.java
@@ -1,65 +1,93 @@
 package englishexercises;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * ¿Dónde está la mosca? Estamos intentando atrapar una mosca. La mosca elegirá
- * aleatoriamente una posición en nuestro array y tendremos que adivinar la
- * posición en la que se encuentra la mosca. El programa le preguntará al
- * usuario la posición en la que cree que está la mosca. Si la posición es
- * correcta, entonces el jugador gana y el programa termina. Si no es correcta,
- * entonces pueden suceder dos cosas:
- * 
- * La mosca está en una posición contigua. Entonces vuela a otra posición. El
- * programa debe decirle al jugador que estaba cerrado pero la mosca cambió su
- * posición.
- * 
- * La mosca no está en una posición contigua, entonces la mosca se queda donde
- * está. El programa debe decirle al jugador que la mosca está lejos de esa
- * posición.
- * 
- * El tamaño del array será 10, pero se puede cambiar.
- */
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
 
-		// Creamos el array tableroMosca como int para almacenar la posción en la que se
-		// encontrara la mosca.
-		int tableroMosca[] = new int[10];
+		// We create the flyBoard array as a String to store the fly.
+		String flyBoard[] = new String[10];
 
-		// Creamos la variable posUsuario como int para almacenar la posición que
-		// introducira el usuario.
-		int posUsuario;
+		// We create the variable userPosition as int to store the position entered by
+		// the user.
+		int userPosition;
 
-		// Creamos la variable posMosca como int para almacenar la posición aleatoria
-		// donde se pondra la mosca.
-		int posMosca;
+		// We create the variable flyPosition as int to store the random position where
+		// the fly will be placed.
+		int flyPosition;
 
-		// Creamos el Scanner para leer la posición de la mosca introducida por el
-		// usuario.
+		// We create the Scanner to read the position of the fly entered by the user.
 		Scanner sc = new Scanner(System.in);
 
-		// Creamos la clase random para elegir aleatoriamente la posición donde se
-		// posará la mosca.
+		// We create the random class to randomly choose the position where the fly will
+		// land.
 		Random rand = new Random();
 
-		// Mostramos un mensaje del juego de atrapar la mosca
-		System.out.println("¡¡BIENVENIDO! Este es el juego de la mosca!");
-		
-		//Generamos una posición aleatoria donde haya caido la mosca por primera vez.
-		posMosca = rand.nextInt(0, 10);
-		
-		// Le pedimos al usuario que introduzca la posición donde cree que se encuentra
-		// la mosca y la leemos.
-		System.out.print("Introduce la posición donde crees que se encuentra la mosca 0 - 9 --> ");
-		posUsuario = sc.nextInt();
-		
-		
-		
-		
-		
+		// We show a message from the game of catching the fly.
+		System.out.println("WELCOME! This is the game of catch the fly");
+
+		// We initialize our array flyBoard to an empty string.
+		Arrays.fill(flyBoard, " ");
+
+		// We generate a random position and store it in flyPosition and store in that
+		// position an asterisk simulating the fly.
+		flyPosition = rand.nextInt(0, 10);
+		flyBoard[flyPosition] = "*";
+
+		// We ask the user to enter the position where he thinks the fly is located and
+		// we read it.
+		System.out.print("Enter the position where you think the fly is located 0 - 9 -->");
+		userPosition = sc.nextInt();
+
+		// We check if the position where the fly is located is different from the one
+		// entered by the user.
+		while (flyPosition != userPosition) {
+
+			// We check if the fly is one position higher or lower than the one entered by
+			// the user. If so, we do the following:
+			if (((userPosition + 1) == flyPosition) || ((userPosition - 1) == flyPosition)) {
+				// We initialize the array to a space because that means the fly has flown to
+				// another place.
+				Arrays.fill(flyBoard, " ");
+
+				// We generate another random position and equate the fly's position (*) to that
+				// position.
+				flyPosition = rand.nextInt(0, 10);
+				flyBoard[flyPosition] = "*";
+
+				// We indicate to the user that the fly was nearby but has changed its position.
+				System.out.println("It was close! You are close but it has flown to another position");
+
+				// If the position is not found one position more or less than that entered by
+				// the user, we show the following message.
+			} else {
+				// We print that the fly is far from the position entered by the user.
+				System.out.println("The fly is far from its position");
+			}
+
+			// We print a line break.
+			System.out.println();
+
+			// We show a message to the user to try again, try another position and read the
+			// response.
+			System.out.println("Please try again --> ");
+			System.out.print("Enter the position where you think the fly is located 0 - 9 -->");
+			userPosition = sc.nextInt();
+		}
+
+		// Line break.
+		System.out.println();
+
+		// Congratulatory message to the user for having discovered the position where
+		// the fly was located.
+		System.out.println("CONGRATULATIONS! You have caught the fly");
+
+		// Closing Scanner
+		sc.close();
+
 	}
 
 }
