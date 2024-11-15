cebfbc414f31ecca4f42299d75291033f130d36d
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2024-11-15 13:43:42 +0100

    Ejercicio 5 - Corregido y completo

--- Tema3/src/boletinpart3/Ejercicio5.java
+++ Tema3/src/boletinpart3/Ejercicio5.java
@@ -15,11 +15,8 @@ public class Ejercicio5 {
 		// para alamcenar 20 números aleatorios.
 		int sumaNumAleatorios[][] = new int[4][5];
 
-		// Creamos la variable sumaFila para almacenar la suma de cada fila.
-		int sumaFila = 0;
-
-		// Creamos la variable sumaColumna para almacenar la suma de cada columna.
-		int sumaColumna = 0;
+		// Creamos la variable suma para almacenar la suma de cada fila y columna.
+		int suma = 0;
 
 		// Creamos la variable sumaTotal para almacenar la suma total de cada fila y
 		// columna.
@@ -28,25 +25,38 @@ public class Ejercicio5 {
 		// Creamos la clase random para generar números aleatorios entre 100 y 999.
 		Random rand = new Random();
 
+		// Creamos el siguiente for para recorrer cada fila de nuestra tabla y
+		// rellenarla con aleatorios.
+		for (int i = 0; i < sumaNumAleatorios.length; i++) {
+
+			// Creamos el siguinte for para recorrer cada columna de nuestra tabla e ir
+			// generando el aleatorio.
+			for (int j = 0; j < sumaNumAleatorios[0].length; j++) {
+				// Asignamos a cada espacio la un valor entre 100 y 999.
+				sumaNumAleatorios[i][j] = rand.nextInt(100, 1000);
+
+			}
+
+		}
+
 		// Creamos el siguiente for para recorrer cada fila de nuestra tabla.
 		for (int i = 0; i < sumaNumAleatorios.length; i++) {
 			// En cada iteración (en cada fila) reiniciamos este valor para que empieze
 			// desde .
-			sumaFila = 0;
-			// Creamos el sigueinte for para recorrer cada columna de nuestra tabla.
+			suma = 0;
+
+			// Creamos el siguiente for para recorrer cada columna de nuestra tabla.
 			for (int j = 0; j < sumaNumAleatorios[0].length; j++) {
-				// Asignamos a cada espacio la un valor entre 100 y 999.
-				sumaNumAleatorios[i][j] = rand.nextInt(100, 1000);
 				// Al valor de sumaFila le vamos sumando cada valor que se va generando.
-				sumaFila += sumaNumAleatorios[i][j];
+				suma += sumaNumAleatorios[i][j];
 				// Imprimimos el número aleatorio generado.
 				System.out.print(sumaNumAleatorios[i][j] + "\t");
 
 			}
 			// A suma total le vamos sumando el total de cada fila.
-			sumaTotal += sumaFila;
+			sumaTotal += suma;
 			// Imprimimos despues de cada fila el total de cada número generado en la fila.
-			System.out.print(sumaFila);
+			System.out.print(suma);
 			// Imprimimos un salto de linea para saltar a otra linea.
 			System.out.println();
 		}
@@ -58,16 +68,16 @@ public class Ejercicio5 {
 		for (int j = 0; j < sumaNumAleatorios[0].length; j++) {
 			// Suma columna lo inicializamos a 0 para en cada iteración el valor se
 			// reinicie.
-			sumaColumna = 0;
+			suma = 0;
 			// Con este for recorremos las filas.
 			for (int i = 0; i < sumaNumAleatorios.length; i++) {
 				// Va sumando cada valor de una columna y lo va almacenando en sumaColumna.
-				sumaColumna += sumaNumAleatorios[i][j];
+				suma += sumaNumAleatorios[i][j];
 
 			}
-			
+
 			// Imprimimos la suma total de cada columna.
-			System.out.print(sumaColumna + "\t");
+			System.out.print(suma + "\t");
 		}
 
 		// Por ultimo imprimimos la sumaTotal que contiene la suma total de cada fila y
