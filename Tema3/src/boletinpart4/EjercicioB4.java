09a8f59f02c79362f3b1154c429fc265998b05c5
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2024-11-20 10:21:35 +0100

    Ejercicio B4 - Completo

--- Tema3/src/boletinpart4/EjercicioB4.java
+++ Tema3/src/boletinpart4/EjercicioB4.java
@@ -11,70 +11,87 @@ public class EjercicioB4 {
 		// Creamos el array tablaMagica para almacenar una serie de valores.
 		int tablaMagica[][] = { { 4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } };
 
-		System.out.println(esMagica(tablaMagica));
+		// Creamos la variable magica como boolean para almacenar el resueltado que nos
+		// devuelve nuestra función esMágica.
+		boolean magica = esMagica(tablaMagica);
+
+		//Mostramos un mensaje
+		System.out.print("¿Es simetrica nuestra tabla? --> ");
 		
+		// Comprobamos si nuestra variable magica es true, eso significa que la tabla es
+		// magica.
+		if (magica) {
+			System.out.println("La tabla es SIMETRICA");
+			// Si no, no es mágica.
+		} else {
+			System.out.println("La tabla es NO SIMETRICA");
+		}
+
 	}
 
+	/**
+	 * Esta función se encarga de comprobar que la suma de columnas y filas es
+	 * siempre la misma.
+	 * 
+	 * @param tabla El array donde estan todos los valores de nuestra tabla.
+	 * @return True si la suma de cada fila es igual a las demas filas y columnas.
+	 *         False en caso contrario.
+	 */
 	public static boolean esMagica(int tabla[][]) {
 
 		// Creamos la variable sol para devolver true o false dependiendo si la tabla es
-		// magica o no.
-		boolean sol = false;
+		// magica o no. Se inicializa a true porque siempre será true a no ser que se
+		// demuestre lo contrario.
+		boolean sol = true;
 
 		// Creamos la variable suma como int para almacenar la suma momentaneamente de
-		// cada fila y columna.
-		int suma = 0;
-		
-		int sumaAnterior = 0;
+		// cada fila.
+		int sumaFila = 0;
 
-		// Creamos la variable indiceBusqueda como int para recorrer cada posicion de
-		// nuestro array.
+		// Creamos la variable suma como int para almacenar la suma momentaneamente de
+		// cada columna.
+		int sumaColumna = 0;
+
+		// Creamos la variable indiceI como int para recorrer cada fila de nuestra
+		// tabla.
 		int indiceI = 0;
+
+		// Creamos la variable indiceJ como int para recorrer cada columna de nuestra
+		// tabla.
 		int indiceJ = 0;
 
-		
-		/*for (int i = 0; i < tabla.length; i++) {
-			suma = 0;
-			for (int j = 0; j < tabla[i].length; j++) {
-				suma+= tabla[i][j];
-			}
-			if (i == 0) {
-				sumaAnterior = suma;
-			}
-			
-			if (sumaAnterior == suma) {
-				sol = true;
-			} else {
-				sol = false;
-			}
-			
-			sumaAnterior = suma;
-			
-		}*/
-		
-		while (indiceI < tabla.length) {
-			suma = 0;
+		// Comprobamos si la suma de una fila y columnas es igual, y indiceI es menor
+		// que la longitud de la tabl.
+		while (sol && indiceI < tabla.length) {
+
+			// Inicializamos cada suma a 0 antes de comprobar otra nueva fila.
+			sumaFila = 0;
+			sumaColumna = 0;
+
 			indiceJ = 0;
-			
+
+			// Comprobamos si el indiceJ es menor que la longitud de la tabla.
 			while (indiceJ < tabla[indiceI].length) {
-				suma+= tabla[indiceI][indiceJ];
+
+				// Calculamos la suma de la primera fila y la primera columna.
+				sumaFila += tabla[indiceI][indiceJ];
+				sumaColumna += tabla[indiceJ][indiceI];
+
+				// Incrementamos el indice de J.
 				indiceJ++;
 			}
-			
-			if (indiceI == 0) {
-				sumaAnterior = suma;
-			}
-			
-			if (sumaAnterior == suma) {
-				sol = true;
-			} else {
+
+			// Comprobamos si la suma de la fila y columna es distinta si el así, devolvemos
+			// false y nos salimos del bucle.
+			if (sumaFila != sumaColumna) {
 				sol = false;
 			}
-			
-			sumaAnterior = suma;
+
+			// Incrementamos el indice de I
 			indiceI++;
 		}
-		
+
+		// Devolvemos si la tabla es mágica o no.
 		return sol;
 	}
 
